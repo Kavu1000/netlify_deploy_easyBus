10af74ad8701f833b6e0516e433b0f0019b634a5
KavuThao <kavuthao2007@KavuThaos-MacBook-Pro.local>
2026-01-18 11:49:01 +0700

    fix(deploy): configure base path and homepage for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/netlify_deploy_easyBus/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
