bcad49791c9ae504fb4a80f2674d0bfdb6672fe5
KavuThao <kavuthao2007@KavuThaos-MacBook-Pro.local>
2026-01-18 22:58:02 +0700

    change api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://easy-bus-backend-1.onrender.com/api',
+    // baseURL: 'https://easy-bus-backend-1.onrender.com/api',
+    baseURL: 'https://bus-api.nkaujntseeg.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
